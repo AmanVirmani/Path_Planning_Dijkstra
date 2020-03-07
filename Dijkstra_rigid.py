941f28ad7db3117c8f881e1ac94ed0b5d2d67eba
Aman Virmani <aman.virmani26@gmail.com>
2020-03-06 19:06:01 -0500

    completed code for rigid robot without collision space

--- Dijkstra_rigid.py
+++ Dijkstra_rigid.py
@@ -1,72 +1,71 @@
 import numpy as np
 import cv2
 import math
+import obstacle_map
 
 def load_map(fname = None):
     if fname is not None :
         map_ = cv2.imread(fname)
         return map_
-    try :
-        map_ = cv2.imread('./map.jpg')
-        return map_
-    except :
-        world= 255*np.ones((200,300,3))
-        rc=0
-        obstacle_map.obstacle_circle(world)
-        obstacle_map.obstacle_ellipse(world)
-        obstacle_map.obstacle_rhombus(world)
-        obstacle_map.obstacle_rectangle(world)
-        obstacle_map.obstacle_polygon(world)
+    world= 255*np.ones((200,300,3))
+    rc=0
+    obstacle_map.obstacle_circle(world)
+    obstacle_map.obstacle_ellipse(world)
+    obstacle_map.obstacle_rhombus(world)
+    obstacle_map.obstacle_rectangle(world)
+    obstacle_map.obstacle_polygon(world)
 
-        cv2.imwrite('./map.jpg',world)
+    cv2.imwrite('./map.jpg',world)
     return world
 
 def isValidNode(map_,x,y,r):
     rows,cols = map_.shape[:2]
-    if 0 <= x < rows and 0 <= y < cols and not detectCollision(map_,(x,y),r):
-        return True
-    else :
+    if 0 <= x-r and x+r < rows and 0 <= y-r and y+r < cols:
+        if not detectCollision(map_, (x, y), r):
+            return True
+    else:
         return False
 
 def detectCollision(img, center,radius):
     for i in range(2*radius+1):
         for j in range(2*radius+1):
             if i**2+j**2 <= radius**2:
-                #if not (img[center[0]+i][center[1]+j]==(255,255,255)).any() and not (img[center[0]+i][center[1]-j]==(255,255,255)).any()\
-                #        and not (img[center[0]-i][center[1]-j]==(255,255,255)).any() and not (img[center[0]-i][center[1]+j]==(255,255,255)).any():
-                #    return True
-                try:
-                    if not (img[center[0]+i][center[1]+j]==(255,255,255)).any() and not (img[center[0]+i][center[1]-j]==(255,255,255)).any()\
-                            and not (img[center[0]-i][center[1]-j]==(255,255,255)).any() and not (img[center[0]-i][center[1]+j]==(255,255,255)).any():
-                        return True
-                except :
-                    return False
-
+                if not (img[center[0]+i][center[1]+j]==(255,255,255)).any() and not (img[center[0]+i][center[1]-j]==(255,255,255)).any()\
+                        and not (img[center[0]-i][center[1]-j]==(255,255,255)).any() and not (img[center[0]-i][center[1]+j]==(255,255,255)).any():
+                    return True
     return False
 
 def getStartNode(map_):
     print("Enter the start co-ordinates")
-    x,y = -1,-1
+    rows, cols= map_.shape[:2]
     while True :
+        ## Cartesian Form
         x = int(input("x_intial is: "))
         y = int(input("y_intial is: "))
-        if not isValidNode(map_,x,y,r) :
+        ## image coordinates
+        row = rows-y-1 ; col = x
+        if not isValidNode(map_, row, col,r):
             print('Input Node not within available map range. Please enter again!')
         else:
             break;
-    return (x, y)
+    #return (row, col)
+    return (col, row)
 
 def getGoalNode(map_):
     print("Enter the goal co-ordinates")
-    x,y = -1,-1
+    rows, cols= map_.shape[:2]
     while True:
+        ## Cartesian Form
         x = int(input("x_goal is: "))
         y = int(input("y_goal is: "))
-        if not isValidNode(map_,x,y,r) :
+        ## image coordinates
+        row = rows-y-1 ; col = x
+        if not isValidNode(map_, row, col, r):
             print('Input Node not within available map range. Please enter again! ')
         else:
             break;
-    return (x, y)
+    #return (row, col)
+    return (col, row)
 
 # A node structure for our search tree
 class Node:
@@ -82,7 +81,7 @@ class Node:
 def updateNeighbours(arr, map_, curr_node,r):
     x,y = curr_node
     ## top node
-    if isValidNode(map_,x-1,y,r):
+    if isValidNode(map_,x-1, y, r):
         if (arr[x][y].cost + 1 < arr[x-1][y].cost):
             arr[x-1][y].cost = arr[x][y].cost + 1
             arr[x-1][y].parent = curr_node
@@ -147,20 +146,20 @@ def tracePath(arr,map_,goal_node,r):
     curr_node = goal_node
     while curr_node is not None:
         img = map_.copy()
-        #img[curr_node[0]][curr_node[1]] = (0,0,255)
         cv2.circle(img, curr_node, r, (0,0,255), -1)
+        cv2.imshow('img',img)
+        cv2.waitKey(0)
         images.append(img)
         curr_node = arr[curr_node[0]][curr_node[1]].parent
-
+    cv2.destroyAllWindows()
     images = images[::-1]
     saveVideo(images,output)
 
 def saveVideo(images,output='path.avi'):
     h,w = images[0].shape[:2]
     out = cv2.VideoWriter(output,cv2.VideoWriter_fourcc('M','J','P','G'), 1, (w,h))
+    images= np.uint8(images)
     for img in images:
-        cv2.imshow('img',img)
-        cv2.waitKey(0);cv2.destroyAllWindows()
         out.write(img)
     out.release()
 
@@ -178,13 +177,13 @@ def main():
     start_node = getStartNode(map_)
     goal_node = getGoalNode(map_)
     rows, cols = map_.shape[:2]
-    arr = [[Node() for j in range(cols)] for i in range(rows)]
+    arr = np.array([[Node() for j in range(cols)] for i in range(rows)])
     arr[start_node[0]][start_node[1]].visited = True
     arr[start_node[0]][start_node[1]].cost = 0
     curr_node = start_node
     while not visitedAll(arr):
         if (curr_node == goal_node):
-            print('found Goal!!')
+            print('found Goal at cost {}!!'.format(arr[goal_node[0]][goal_node[1]].cost))
             tracePath(arr,map_,goal_node,r)
             break
         arr[curr_node[0]][curr_node[1]].visited = True
@@ -193,5 +192,7 @@ def main():
 
 if __name__=='__main__':
     main()
-    #print('start_node is : {}'.format(getStartNode()))
-    #rint('goal_node is : {}'.format(getGoalNode()))
+    ## TODO: add exploration video : Nidhi
+    ## TODO: add image for traced path : Aman/Nidhi
+    ## TODO: add collision space : Aman
+    ## TODO: take r, c from user : Aman
