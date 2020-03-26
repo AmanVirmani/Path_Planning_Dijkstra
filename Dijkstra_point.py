0729bbf4703dc2dabf20726defbcc6af9fa98f38
Aman Virmani <aman.virmani26@gmail.com>
2020-03-25 22:13:02 -0400

    updated code
    optimization included;gif generated for outputs

--- Dijkstra_point.py
+++ Dijkstra_point.py
@@ -4,6 +4,8 @@ import math
 import obstacle_map
 import time
 from queue import PriorityQueue
+import argparse
+import imageio
 
 def load_map(fname = None):
     if fname is not None :
@@ -20,6 +22,7 @@ def load_map(fname = None):
     cv2.imwrite('./map.jpg',world)
     return world
 
+
 def isValidNode(map_,x,y):
     rows,cols = map_.shape[:2]
     if 0 <= x < rows and 0 <= y < cols and (map_[x][y]==(255,255,255)).all():
@@ -27,35 +30,23 @@ def isValidNode(map_,x,y):
     else :
         return False
 
-def getStartNode(map_):
-    print("Enter the start co-ordinates")
-    rows, cols= map_.shape[:2]
-    while True :
-        ## Cartesian Form
-        x = int(input("x_intial is: "))
-        y = int(input("y_intial is: "))
-        ## image coordinates
-        row = rows-y-1 ; col = x
-        if not isValidNode(map_, row, col):
-            print('Input Node not within available map range. Please enter again!')
-        else:
-            break;
-    return (row, col)
 
-def getGoalNode(map_):
-    print("Enter the goal co-ordinates")
-    rows, cols= map_.shape[:2]
-    while True:
-        ## Cartesian Form
-        x = int(input("x_goal is: "))
-        y = int(input("y_goal is: "))
-        ## image coordinates
-        row = rows-y-1 ; col = x
-        if not isValidNode(map_, row, col):
-            print('Input Node not within available map range. Please enter again! ')
-        else:
-            break;
-    return (row, col)
+def getStartNode(map_, loc):
+    rows, cols = map_.shape[:2]
+    row = rows-loc[1]-1; col = loc[0]
+    if not isValidNode(map_, row, col):
+        print('Start Node not within available map range. Please enter again!')
+        return None
+    return row, col
+
+
+def getGoalNode(map_, loc):
+    rows, cols = map_.shape[:2]
+    row = rows-loc[1]-1; col = loc[0]
+    if not isValidNode(map_, row, col):
+        print('Goal Node not within available map range. Please enter again! ')
+        return None
+    return row, col
 
 # A node structure for our search tree
 class Node:
@@ -138,29 +129,36 @@ def findMinCost(arr):
                 min_cost = arr[row][col].cost
     return curr_node
 
-def tracePath(arr,map_,goal_node):
-    images= []
-    output = './output.avi'
+
+def tracePath(arr, img, goal_node):
+    nodes = []
+    images = []
+    output = './output.gif'
     curr_node = goal_node
-    img = map_.copy()
+    #img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     while curr_node is not None:
-        img[curr_node[0]][curr_node[1]] = (0,0,255)
-        images.append(img)
-        img[curr_node[0]][curr_node[1]] = (0,0,0)
+        nodes.append(curr_node)
         curr_node = arr[curr_node[0]][curr_node[1]].parent
+    nodes = nodes[::-1]
+    for curr_node in nodes:
+        #img_curr = cv2.circle(img, (curr_node[1], curr_node[0]), 3, (255, 0, 0), -1)
+        img[curr_node[0]][curr_node[1]] = (0, 0, 255)
+        images.append(np.uint8(img.copy()))
+        img[curr_node[0]][curr_node[1]] = (0, 0, 0)
 
-    images = images[::-1]
-    saveVideo(images,output)
+    imageio.mimsave(output, images,fps=55)
+    #saveVideo(images,output)
 
-def saveVideo(images,output='path.avi'):
+def saveVideo(images,output='path.gif'):
     h,w = images[0].shape[:2]
-    out = cv2.VideoWriter(output,cv2.VideoWriter_fourcc('M','J','P','G'), 1, (w,h))
-    images= np.uint8(images)
-    for img in images:
-        cv2.imshow('path traced',img)
-        cv2.waitKey(10)
-        out.write(img)
-    out.release()
+    imageio.mimsave(output, images)
+    #out = cv2.VideoWriter(output,cv2.VideoWriter_fourcc('M','J','P','G'), 1, (w,h))
+    #for img in images:
+    #    img = np.uint8(img)
+    #    #cv2.imshow('path traced', img)
+    #    #cv2.waitKey(10)
+    #    out.write(img)
+    #out.release()
 
 def exploredPath(map_,arr):
     img = map_.copy()
@@ -168,14 +166,16 @@ def exploredPath(map_,arr):
     for row in range(rows):
         for col in range(cols):
             if arr[row][col].visited:
-                img[row][col]==(0,255,255)
+                img[row][col] == (0, 255, 255)
     return img
 
-def main():
+def main(args):
     map_ = load_map()
-    start_node = getStartNode(map_)
-    goal_node = getGoalNode(map_)
     rows, cols = map_.shape[:2]
+    start_node = getStartNode(map_, args['start_node'])
+    goal_node = getStartNode(map_, args['goal_node'])
+    if not (start_node and goal_node):
+        exit(1)
     arr = np.array([[Node() for j in range(cols)] for i in range(rows)])
     arr[start_node[0]][start_node[1]].visited = True
     arr[start_node[0]][start_node[1]].cost = 0
@@ -187,19 +187,32 @@ def main():
     img[goal_node[0]][goal_node[1]] = (0,0,255)
     while queue:
         curr_node = queue.get()[1]
-        if (curr_node == goal_node):
+        if curr_node == goal_node:
             algo_time = time.time()
             print('found Goal in {}s at cost {}!!'.format(algo_time-start_time, arr[goal_node[0]][goal_node[1]].cost))
-            tracePath(arr,map_,goal_node)
+            tracePath(arr, img, goal_node)
             break
         arr[curr_node[0]][curr_node[1]].visited = True
-        arr = updateNeighbours(arr, map_, curr_node,queue)
-        img[curr_node[0]][curr_node[1]] = (0,255,0)
-        #exploredList.append(img)
-        cv2.imshow('explored',img)
-        cv2.waitKey(10)
-    #saveVideo(exploredList,'explored.avi')
+        arr = updateNeighbours(arr, map_, curr_node, queue)
+        img[curr_node[0]][curr_node[1]] = (0, 255, 0)
+        exploredList.append(np.uint8(img.copy()))
+        #cv2.imshow('explored', img)
+        #cv2.waitKey(1)
+    #saveVideo(exploredList,'explored.gif')
+    imageio.mimsave('explored.gif', exploredList, fps=55)
     cv2.destroyAllWindows()
 
+
 if __name__=='__main__':
-    main()
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-r", "--radius", required=False, help="Radius of the robot",
+                    default=1, type=int)
+    ap.add_argument("-c", "--clearance", required=False, help="desired clearance to avoid obstacles",
+                    default=1, type=int)
+    ap.add_argument("-s", "--start_node", required=True, help="Location (x,y) of start node",
+                    nargs='+', type=int)
+    ap.add_argument("-g", "--goal_node", required=True, help="Location (x,y) of goal node",
+                    nargs='+', type=int)
+    args = vars(ap.parse_args())
+    #print(args)
+    main(args)
